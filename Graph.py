364966dc63d6db59cbb274d6e8e041bb3abe45e6
lubgade <lubgade@yahoo.com>
2017-09-08 23:40:12 -0400

    feat: Add a method to get the adjacency list in the graph

--- Graph.py
+++ Graph.py
@@ -58,8 +58,17 @@ class Graph(object):
         Each section in the list will store a list
         of tuples that looks like this:
         (To Node, Edge Value)"""
+        max_index = len(self.nodes) + 1
+        adjacency_list = [None] * max_index
+        for edge_object in self.edges:
+            if adjacency_list[edge_object.node_from.value]:
+                adjacency_list[edge_object.node_from.value].append(
+                    (edge_object.node_to.value, edge_object.value))
+            else:
+                adjacency_list[edge_object.node_from.value] = [
+                    (edge_object.node_to.value, edge_object.value)]
+        return adjacency_list
 
-        return []
 
     def get_adjacency_matrix(self):
         """Return a matrix, or 2D list.
@@ -78,7 +87,7 @@ graph.insert_edge(103, 3, 4)
 # Should be [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
 print graph.get_edge_list()
 # Should be [None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None]
-# print graph.get_adjacency_list()
+print graph.get_adjacency_list()
 # Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
 
 # print graph.get_adjacency_matrix()
